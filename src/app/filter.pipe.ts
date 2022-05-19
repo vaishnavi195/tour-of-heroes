e0e938521d43ed9addd30e9e8285f6901c62314e
vaishnavi195 <varshitha.345@gmail.com>
2022-05-19 18:01:26 +0530

    4 files modified

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,14 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   transform(value: any, filteredString: string) {
-    if (value.length === 0) {
+    if (value.length === 0 || filteredString === '') {
       return value;
     }
     const users = [];
     for (const user of value) {
-      if (user['name'] == 'filteredString') {
-        users.push(value);
+      if (user['name'] === filteredString) {
+        users.push(user);
       }
     }
+    return users;
   }
 }
